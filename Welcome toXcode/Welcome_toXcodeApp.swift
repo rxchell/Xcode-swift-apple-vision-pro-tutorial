9b954f42e4d988bcc16d9cf3c9a4ba3ef9764c61
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2025-08-05 11:09:22 +0800

    Initial Commit

---
+++ Welcome toXcode/Welcome_toXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_toXcodeApp.swift
+//  Welcome toXcode
+//
+//  Created by Rachel  on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_toXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
